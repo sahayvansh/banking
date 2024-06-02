504355952a830d3b9526040d223721cf0d133109
sahayvansh <vansh.sahay@learner.manipal.edu>
2024-06-02 18:06:33 +0530

    nav modified

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import {
     Sheet,
@@ -8,8 +10,13 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 import Image from 'next/image'
+import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
   
 const MobileNavbar = ({user}:MobileNavProps) => {
+    const pathname=usePathname();   
   return (
     <section className='w-full max-w-[264px]'>
         <Sheet>
@@ -22,14 +29,33 @@ const MobileNavbar = ({user}:MobileNavProps) => {
                     className='cursor-pointer'
                 />
             </SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                    This action cannot be undone. This will permanently delete your account
-                    and remove your data from our servers.
-                </SheetDescription>
-                </SheetHeader>
+            <SheetContent side="left" className='border-none bg-white'>
+                <Link href="/" className='flex cursor-pointer items-center gap-3 p-4'>
+                    <Image src="/icons/logo.svg" width={34} height={34} alt="pennywise logo"/>
+                    <h1 className='text-24 font-ibm-plex-serif font-bold text-black-1'>
+                        Pennywise
+                    </h1>
+                </Link>
+                {sidebarLinks.map((item)=> {
+                    const isActive=pathname===item.route || pathname.startsWith(`${item.route}/`)
+                    return(
+                        <Link href={item.route} key={item.label} className={cn('sidebar-link',{'bg-bank-gradient':isActive})}>
+                        <div className='relative size-6'>
+                            <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            fill
+                            className={cn({
+                                'brightness-[3] invert-0':isActive
+                            })}
+                            />
+                        </div>
+                        <p className={cn('sidebar-label', {'!text-white':isActive})}>
+                            {item.label}
+                        </p>
+                        </Link>
+                    )
+                })}
             </SheetContent>
         </Sheet>
 
